c0b74d158e7258fad40613d0e1e386ca2dcdf85c
Lukas Moravec 4 <lukas.moravec4@firma.seznam.cz>
2021-09-29 09:33:45 +0200

    initial

---
+++ prednasky.md
@@ -0,0 +1,30 @@
+
+
+## Typy dat
+Dělíme na numerická a kategoriální
+
+### Numerická
+ * Matematická - dají se s nimi dělat mat. operace
+
+
+### Kategoriální
+* Nominální - nedaji se moc porovnávat
+* Ordinálni - daji se porovnat
+
+## Data Matrix
+Řádky a sloupce, sloupce můžeme nazývat atributy.
+Objekty jsou jakoby řádky v matici.
+
+Jeden řádek matice reprezentuje bod v dimenzi, např. dimenze 2d bude rovina, 3d bude prostor, 4d bod v dimenzi 4.
+
+Sloupec bude reprezentovat vektor, s dimenzí N.
+Práci s maticemi můžeme reprezentovat jako práci s body v prostoru.
+
+S bodama v prostoru můžeme vidět blízkost bodů, skupiny těch bodů, tzv. shluky, odchylky, body které se liší něčím od ostatních *(outlier)*.
+
+Do prostoru si uděláme souřadný systém zavedením os, 1 pro každou dimenzi, které se protnou v jednom bodu 
+a každá osa bude mít i své měřítko. Kartézský souřadnicový systém má osy na sebe kolmé a
+mají stejné měřítko.
+
+Jakýkoliv bod lze vyjádřit jako lineární kombinaci bázových vektorů prostoru.
+
---
+++ process.py
@@ -0,0 +1,132 @@
+from enum import Enum
+import enum
+
+
+class Outlook(Enum):
+    EMPTY = 0
+    SUNNY = 1
+    OVERCAST = 2
+    RAINY = 3
+
+
+class Temp(Enum):
+    EMPTY = 0
+    HOT = 1
+    MILD = 2
+    COOL = 3
+
+
+class Humidity(Enum):
+    EMPTY = 0
+    HIGH = 1
+    NORMAL = 2
+
+
+class Windy(Enum):
+    EMPTY = 0
+    TRUE = 1
+    FALSE = 2
+
+
+class Play(Enum):
+    TRUE = 1
+    FALSE = 2
+
+
+attributes = [
+    ['', 'sunny', 'overcast', 'rainy'],
+    ['', 'hot', 'mild', 'cool'],
+    ['', 'high', 'normal'],
+    ['', 'TRUE', 'FALSE'],
+]
+
+DATA_RULES = """sunny,hot,high,FALSE,no
+sunny,hot,high,TRUE,no
+overcast,hot,high,FALSE,yes
+rainy,mild,high,FALSE,yes
+rainy,cool,normal,FALSE,yes
+rainy,cool,normal,TRUE,no
+overcast,cool,normal,TRUE,yes
+sunny,mild,high,FALSE,no
+sunny,cool,normal,FALSE,yes
+rainy,mild,normal,FALSE,yes
+sunny,mild,normal,TRUE,yes
+overcast,mild,high,TRUE,yes
+overcast,hot,normal,FALSE,yes
+rainy,mild,high,TRUE,no"""
+
+"""
+if sunny and humidity = high => play = no
+if rainy and windy = true => play = no
+if overcast => play = yes
+if humidity = normal => play = yes
+if none of the above => play = yes
+"""
+
+"""
+supp 
+r = pocet, kolikrat se dane atributy (i v kombinaci) nachazi v celkove sade
+N = pocty pravidel v datasetu
+
+conf
+cmax = pocet prevazujici vetsiny (spravne, spatne) kombinaci atributu (=pravidlo), porovnat vuci DATA_RULES
+r = 
+
+"""
+
+
+def gen_data(w_data: list, start: int, end: int, row: list) -> None:
+    for x in attributes[start]:
+        if start == end:
+            w_data.append(row + [x])
+        else:
+            gen_data(w_data, start + 1, end, row + [x])
+
+
+def calc_r(curr: int, rules: list) -> int:
+    r = 0
+    current_attrs = [x for x in rules[curr] if x]
+    for i in range(len(rules)):
+        is_same = all(item in rules[i] for item in current_attrs)
+        r += 1 if is_same else 0
+    return r
+
+
+def get_cmax(rule: list) -> int:
+    cyes = 0
+    cno = 0
+    rule_attrs = [attr for attr in rule if attr]
+    for d_rule in DATA_RULES:
+        matches = all(item in d_rule for item in rule_attrs)
+        if matches:
+            if d_rule[-1] == 'yes':
+                cyes += 1
+            else:
+                cno += 1
+        pass
+
+    return max(cyes, cno)
+
+
+def process():
+    w_data = []
+
+    gen_data(w_data, 0, len(attributes) - 1, [])
+    n = len(w_data)
+    for i, rule in enumerate(w_data):
+        r = calc_r(i, w_data)
+        cmax = get_cmax(rule)
+        print(f'{i + 1}: {rule}, supp: {r / n:.2f}, cmax: {cmax}, conf: {cmax / r:.2f}')
+
+
+    # subvalues = [item in w_data[10] for item in w_data[12] if item]
+    # print(subvalues)
+    # print(all(subvalues))
+
+    pass
+
+
+if __name__ == '__main__':
+    DATA_RULES = DATA_RULES.split('\n')
+    DATA_RULES = [rule.split(',') for rule in DATA_RULES]
+    process()
