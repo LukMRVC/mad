a17f5f3b05673144a6a7cb4c29ee907920b7bbb4
Lukas Moravec <lukas.moravec21@seznam.cz>
2021-12-09 17:53:25 +0100

    Downloading graph

--- project/main.ts
+++ project/main.ts
@@ -9,10 +9,19 @@ router
         const startNodes = parseInt(params.get('nodes') || '', 10);
         const steps = parseInt(params.get('steps') || '', 10);
         const connectionParam = parseInt(params.get('connection') || '', 10);
+        const download = params.has('download');
 
         try {
-            context.response.headers.set('Content-Type', 'json');
-            context.response.body = BarabasiAlbert(startNodes, steps, connectionParam);
+            const graph = BarabasiAlbert(startNodes, steps, connectionParam);
+            if (download) {
+                context.response.headers.set('Content-Type', 'text/plain');
+                context.response.headers.set('Content-Length', JSON.stringify(graph).length.toString());
+                context.response.headers.set('Content-Transfer-Encoding', 'text');
+                context.response.headers.set('Content-Disposition', 'attachment; filename=graph.txt');
+            } else {
+                context.response.headers.set('Content-Type', 'json');
+            }
+            context.response.body = graph;
         } catch (error) {
             context.response.status = 400;
             context.response.body = error.message;
